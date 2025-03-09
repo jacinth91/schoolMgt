6c654e94be10b75b6930e30e6883d110f2505f22
JacinthV <joy237@gmail.com>
2025-03-09 21:05:31 +0530

    navbar

--- src/App.js
+++ src/App.js
@@ -1,10 +1,7 @@
 import React, { Fragment, useEffect } from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Navbar from "./components/layout/Navbar";
-import Login from "./components/auth/Login";
-import Register from "./components/auth/Register";
-import Landing from "./components/layout/Landing";
-import PrivateRoute from "./components/routing/PrivateRoute";
+
 // Redux
 import { Provider } from "react-redux";
 import store from "./store";
@@ -13,6 +10,7 @@ import setAuthToken from "./utils/setAuthToken";
 
 import "./App.css";
 import Dashboard from "./components/parenPortal/dashboard";
+import Footer from "./components/layout/footer";
 
 if (localStorage.token) {
   setAuthToken(localStorage.token);
@@ -28,12 +26,14 @@ const App = () => {
       <Router>
         {/* <Fragment> */}
          {/* <Navbar /> */}
+         <Navbar/>
           <Routes>
             <Route exact path="/dashboard" element={<Dashboard />} />
             {/* <Route exact path="/register" element={<Register />} />
             <Route exact path="/login" element={<Login />} /> */}
           </Routes>
         {/* </Fragment> */}
+        <Footer />
       </Router>
     </Provider>
   );
