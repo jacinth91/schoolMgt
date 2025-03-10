039ad7839cc71db220e1b70cb5deb5d83f99a729
JacinthV <joy237@gmail.com>
2025-03-10 21:22:11 +0530

    labels and css

--- src/components/layout/Navbar.js
+++ src/components/layout/Navbar.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import { BarChart3, Menu } from "lucide-react";
+import { BarChart3, Menu ,UserPlus} from "lucide-react";
 
 const Navbar = () => {
   return (
@@ -28,24 +28,40 @@ const Navbar = () => {
               </li>
               <li className="nav-item">
                 <Link className="nav-link" to="/analytics">
-                  Analytics
+                  Profile
                 </Link>
               </li>
               <li className="nav-item">
                 <Link className="nav-link" to="/reports">
-                  Reports
+                  Children
                 </Link>
               </li>
               <li className="nav-item">
                 <Link className="nav-link" to="/settings">
-                  Settings
+                  Products
                 </Link>
+              </li>
+              <li className="nav-item">
+                <Link className="nav-link" to="/settings">
+                  My cart
+                </Link>
+              </li>
+              <li className="nav-item">
+                <Link className="nav-link" to="/settings">
+                  Support
+                </Link>
+              </li>
+              <li>
+              <button className="add-child-button">
+      <UserPlus className="button-icon" />
+      <span>Add Child</span>
+    </button>
               </li>
             </ul>
           </div>
         </div>
       </nav>
-      <div className="welcome-wrapper align-content-end">
+      {/* <div className="welcome-wrapper align-content-end">
         <div className="px-4 py-3 w-50 welcome-box">
           <div className="ps-5 ms-3">
             <h3>Why Parents Choose Our Software</h3>
@@ -55,7 +71,7 @@ const Navbar = () => {
             </p>
           </div>
         </div>
-      </div>
+      </div> */}
     </div>
   );
 };
